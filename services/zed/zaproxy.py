b1724bbc0e3c6527a64a064f1e7d5975c22dcbec
mendas-cpu <omarmendas2018@gmail.com>
2026-04-12 18:00:25 +0100

    fix: active scan is added to scan for traffic vulns

--- services/zed/zaproxy.py
+++ services/zed/zaproxy.py
@@ -18,9 +18,9 @@ class ZapScanner:
         self.scan_id = self.zap.ajaxSpider.scan(self.target)
         start_time = time.time()
         while self.zap.ajaxSpider.status != 'stopped':
-            if time.time() - start_time > 120:  # 2 minute timeout
+            if time.time() - start_time > 120:  # 2 minutes timeout
                 self.zap.ajaxSpider.stop()
-                print("Ajax spider timed out, stopping...")
+                print("Ajax spider timed out.")
                 break
             print('Ajax Spider status: ' + self.zap.ajaxSpider.status)
             time.sleep(2)
@@ -38,34 +38,48 @@ class ZapScanner:
             time.sleep(5)
         print("Active scan completed")
         return self.zap.alert.alerts(baseurl=self.target, start=0, count=5000)
-
-    #returning the alerts dict after initiating exploration and active scanning
-    def results(self):
-        self.exploring_app()
-        alert_dict = {}
-        alert_count = 0
+    def collect_alerts(self):
+        alert_dict = []
         st = 0
-        pg = 5000
+        pg = 500
         blacklist = [1, 2]
         alerts = self.zap.alert.alerts(baseurl=self.target, start=st, count=pg)
 
         while len(alerts) > 0:
             print('Reading {} alerts from {}'.format(pg, st))
             for alert in alerts:
-                plugin_id = alert.get('pluginId')
+                plugin_id = int(alert.get('pluginId'))
                 if plugin_id in blacklist:
                     continue
                 if alert.get('risk') == 'Informational':
                     continue
-                alert_dict[alert_count] = {
+                alert_dict.append({
                     "risk": alert.get('risk'),
                     "name": alert.get('name'),
                     "url": alert.get('url'),
                     "description": alert.get('description'),
                     "solution": alert.get('solution')
-                }
-                alert_count += 1
+                })
             st += pg
             alerts = self.zap.alert.alerts(baseurl=self.target, start=st, count=pg)
 
         return alert_dict
+
+    #returning the alerts arr after initiating exploration and active scanning
+    def results(self):
+        self.exploring_app()
+        scan_id = self.zap.spider.scan(self.target)
+        while int(self.zap.spider.status(scan_id)) < 100:
+            print("Spider progress:", self.zap.spider.status(scan_id))
+            time.sleep(2)
+
+        while int(self.zap.pscan.records_to_scan) > 0:
+            print("Remaining:", self.zap.pscan.records_to_scan)
+            time.sleep(2)
+
+        print("Starting active scan")
+        self.active_scan()
+
+        print("Collecting alerts")
+        return self.collect_alerts()
+
